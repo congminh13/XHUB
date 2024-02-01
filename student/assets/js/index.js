9e77df59005c460525ca5777ebb3b6dd27f5c1fa
trancongminh150808@gmail.com <trancongminh150808@gmail.com>
2024-02-01 19:59:07 +0700

    commit 35, update lil code

--- student/assets/js/index.js
+++ student/assets/js/index.js
@@ -1,25 +1,3 @@
-// import { getLocalStorage } from '../../../assets/js/tools.js';
-// import { lang_count } from '../../../assets/lang/lang_count.js';
-
-// let globalLogData = getLocalStorage("GLOBAL_LOG_DATA");
-// const lang = globalLogData.lang;
-// let global_lang = '';
-
-// for (let i of lang_count) {
-//     if (lang == i) {
-//         global_lang = i; 
-//         break;
-//     }
-// }
-
-// const lang_path = `../../../assets/lang/${global_lang}/lang_std/lang.json`;
-// console.log(lang_path);
-// import(lang_path)
-//     .then(module => {
-//         const data = module.default; 
-//         console.log(data);
-//     })
-
 import { renderOverview } from './overview-render.js';
 
 const renderIndex = (container) => {
@@ -41,3 +19,10 @@ const renderCenterContent = () => {
     centerElements.appendChild(centerContainer);
 };
 renderCenterContent();
+
+const checkout = document.getElementById("logout-button");
+
+checkout.addEventListener("click", (event) => {
+    localStorage.removeItem("GLOBAL_LOG_DATA");
+ });
+ 
